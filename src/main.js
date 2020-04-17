51f59171545c48c5a0b3086cec1e517dd1771522
Joakim Olsson <barbatron@gmail.com>
2020-04-17 15:41:55 +0000

    scripts, refac, some stuff

--- src/main.js
+++ src/main.js
@@ -1,93 +1,63 @@
-/**
- * Read Linux mouse(s) in node.js
- * Author: Marc Loehe (marcloehe@gmail.com)
- *
- * Adapted from Tim Caswell's nice solution to read a linux joystick
- * http://nodebits.org/linux-joystick
- * https://github.com/nodebits/linux-joystick
- */
+const {Mouse} = require('./mouse.js');
 
-var fs = require('fs'),
-    EventEmitter = require('events').EventEmitter;
+const mouse = new Mouse();
 
-/**
- * Parse PS/2 mouse protocol
- * According to http://www.computer-engineering.org/ps2mouse/
- */
-function parse(mouse, buffer) {
-  var event = {
-    leftBtn:    (buffer[0] & 1  ) > 0, // Bit 0
-    rightBtn:   (buffer[0] & 2  ) > 0, // Bit 1
-    middleBtn:  (buffer[0] & 4  ) > 0, // Bit 2
-    xSign:      (buffer[0] & 16 ) > 0, // Bit 4
-    ySign:      (buffer[0] & 32 ) > 0, // Bit 5
-    xOverflow:  (buffer[0] & 64 ) > 0, // Bit 6
-    yOverflow:  (buffer[0] & 128) > 0, // Bit 7
-    xDelta:      buffer.readInt8(1),   // Byte 2 as signed int
-    yDelta:      buffer.readInt8(2)    // Byte 3 as signed int
-  };
-  if (event.leftBtn || event.rightBtn || event.middleBtn) {
-    event.type = 'button';
-  } else {
-    event.type = 'moved';
+// Spans deduced from previous runs
+const X_SPAN = 510; 
+const Y_SPAN = 382;
+
+let x = X_SPAN / 2;
+let y = Y_SPAN / 2;
+
+let count = 0;
+
+let minx = x;
+let miny = y;
+let maxx = x;
+let maxy = y;
+
+let lastEvent;
+
+const AUTOCENTER_FACTOR = 0.8;
+const AUTOCENTER_INTERVAL = 2000;
+
+const trunc = (value) => Math.floor(value);
+const autoCenter = (old) => trunc(old * AUTOCENTER_FACTOR);
+
+const show = () => {
+  const {type, xDelta, yDelta} = lastEvent;
+  const xSpan = trunc(Math.abs(maxx - minx));
+  const ySpan = trunc(Math.abs(maxy - miny));
+  console.clear();
+  console.log('Event count: ', count);
+  console.table({x, y, xDelta, yDelta, maxx, minx, miny, maxy, xSpan, ySpan});
+  console.log();
+  console.log(lastEvent);
+};
+
+setInterval(() => {
+  x = autoCenter(x);
+  y = autoCenter(y);
+  minx = autoCenter(minx);
+  maxx = autoCenter(maxx);
+  miny = autoCenter(miny);
+  maxy = autoCenter(maxy);
+  if (lastEvent) { 
+    show();
   }
-  return event;
-}
+}, AUTOCENTER_INTERVAL);
 
-function Mouse(mouseid) {
-  this.wrap('onOpen');
-  this.wrap('onRead');
-  this.dev = typeof(mouseid) === 'number' ? 'mouse' + mouseid : 'mice';
-  this.buf = new Buffer(3);
-  fs.open('/dev/input/' + this.dev, 'r', this.onOpen);
-}
-
-Mouse.prototype = Object.create(EventEmitter.prototype, {
-  constructor: {value: Mouse}
-});
-
-Mouse.prototype.wrap = function(name) {
-  var self = this;
-  var fn = this[name];
-  this[name] = function (err) {
-    if (err) return self.emit('error', err);
-    return fn.apply(self, Array.prototype.slice.call(arguments, 1));
-  };
+const onMove = (ev) => {
+ count++;
+ lastEvent = ev;
+ const {type, xDelta, yDelta} = ev;
+ x += xDelta;
+ y += yDelta;
+ minx = Math.min(x, minx);
+ maxx = Math.max(x, maxx);
+ miny = Math.min(y, miny);
+ maxy = Math.max(y, maxy);
+ show();
 };
 
-Mouse.prototype.onOpen = function(fd) {
-  this.fd = fd;
-  this.startRead();
-};
-
-Mouse.prototype.startRead = function() {
-  fs.read(this.fd, this.buf, 0, 3, null, this.onRead);
-};
-
-Mouse.prototype.onRead = function(bytesRead) {
-  var event = parse(this, this.buf);
-  event.dev = this.dev;
-  this.emit(event.type, event);
-  if (this.fd) this.startRead();
-};
-
-Mouse.prototype.close = function(callback) {
-  fs.close(this.fd, (function(){console.log(this);}));
-  this.fd = undefined;
-}
-
-/****************
- * Sample Usage *
- ****************/
-
-// read all mouse events from /dev/input/mice
-var mouse = new Mouse();
-mouse.on('button', console.log);
-mouse.on('moved', console.log);
-
-// to read only a specific mouse by id (e.g. /dev/input/mouse0) use
-// var mouse0 = newMouse(0);
-
-// to close mouse
-// mouse.close();
-// mouse = undefined;
+mouse.on('mice', onMove);
