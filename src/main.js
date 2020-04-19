6cd56801f53d0f7bf362e6c77e0e8798c82cf2f7
Joakim Olsson <barbatron@gmail.com>
2020-04-19 00:24:44 +0000

    Split things, added i2c-´led

--- src/main.js
+++ src/main.js
@@ -1,63 +1,25 @@
-const {Mouse} = require('./mouse.js');
+const { getPwm } = require('./i2c-led');
 
-const mouse = new Mouse();
+const { run } = require('./eventdump');
 
-// Spans deduced from previous runs
-const X_SPAN = 510; 
-const Y_SPAN = 382;
+const testPca9685 = async () => {
+	const pcaAddr = 0x41;
+	const pwmChan = 15;
 
-let x = X_SPAN / 2;
-let y = Y_SPAN / 2;
+	try {
+		const pwm = await getPwm(1, pcaAddr, 1024);
+		console.log('Initialization done');
+		console.log('Things in pwm ', Object.keys(pwm));
 
-let count = 0;
+		pwm.channelOn(pwmChan);
 
-let minx = x;
-let miny = y;
-let maxx = x;
-let maxy = y;
-
-let lastEvent;
-
-const AUTOCENTER_FACTOR = 0.8;
-const AUTOCENTER_INTERVAL = 2000;
-
-const trunc = (value) => Math.floor(value);
-const autoCenter = (old) => trunc(old * AUTOCENTER_FACTOR);
-
-const show = () => {
-  const {type, xDelta, yDelta} = lastEvent;
-  const xSpan = trunc(Math.abs(maxx - minx));
-  const ySpan = trunc(Math.abs(maxy - miny));
-  console.clear();
-  console.log('Event count: ', count);
-  console.table({x, y, xDelta, yDelta, maxx, minx, miny, maxy, xSpan, ySpan});
-  console.log();
-  console.log(lastEvent);
+		setInterval(() => {
+			pwm.setDutyCycle(pwmChan, Math.random());
+		}, 100);
+	} catch (err) {
+		console.error('Error initializing PCA9685', err);
+	}
 };
 
-setInterval(() => {
-  x = autoCenter(x);
-  y = autoCenter(y);
-  minx = autoCenter(minx);
-  maxx = autoCenter(maxx);
-  miny = autoCenter(miny);
-  maxy = autoCenter(maxy);
-  if (lastEvent) { 
-    show();
-  }
-}, AUTOCENTER_INTERVAL);
-
-const onMove = (ev) => {
- count++;
- lastEvent = ev;
- const {type, xDelta, yDelta} = ev;
- x += xDelta;
- y += yDelta;
- minx = Math.min(x, minx);
- maxx = Math.max(x, maxx);
- miny = Math.min(y, miny);
- maxy = Math.max(y, maxy);
- show();
-};
-
-mouse.on('mice', onMove);
+// testPca9685();
+run();
